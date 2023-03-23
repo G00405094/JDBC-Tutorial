123e023d2e4a968931698c06408091a0e6ebec6e
G00405094 <G00405094@atu.ie>
2023-03-23 12:02:51 +0000

    New class insert Connects to database

--- src/main/java/ie/atu/Insert.java
+++ src/main/java/ie/atu/Insert.java
@@ -1,4 +1,38 @@
 package ie.atu;
 
+import java.sql.*;
+
 public class Insert {
+    public static void main(String[] args) throws SQLException{
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/exampledatabase", "root", "root");
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
+            stmt.setString(1, "Sean");
+            stmt.setString(2, "Software Engineering");
+            stmt.executeUpdate();
+
+            stmt = conn.prepareStatement("INSERT INTO emails (user_id, email) VALUES (?, ?)");
+            stmt.setInt(1, getLastInsertId(conn));
+            stmt.setString(2, "SeanGrehan@atu.ie");
+            stmt.executeUpdate();
+
+            System.out.println("Insert completed successfully.");
+        }
+        catch (SQLException ex){
+
+            System.out.println("Record insert failed");
+            ex.printStackTrace();
+        }
+        conn.close();
+    }
+    private static int getLastInsertId(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
+        rs.next();
+        int id = rs.getInt(1);
+        rs.close();
+        stmt.close();
+        return id;
+    }
 }
